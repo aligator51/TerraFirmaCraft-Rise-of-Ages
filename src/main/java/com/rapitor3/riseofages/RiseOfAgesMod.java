2a0296fa4cf8c9644416e1fd898f3d0e102601d2
Danil Bubekov <bubekov.work@gmail.com>
2026-04-11 00:16:45 +0300

    Refactor project structure and remove examplemod things

---
+++ src/main/java/com/rapitor3/riseofages/RiseOfAgesMod.java
@@ -0,0 +1,13 @@
+package com.rapitor3.riseofages;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(RiseOfAgesMod.MODID)
+public class RiseOfAgesMod {
+
+    public static final String MODID = "riseofages";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+}
